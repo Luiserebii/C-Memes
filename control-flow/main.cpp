a9b9bb6a3045f589d9837217805b1b2add531c5c
Luiserebii <luis@serebii.io>
2019-11-29 22:50:09 -0500

    Fleshing code out for input and rolling

--- control-flow/main.cpp
+++ control-flow/main.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define IN_LIMIT 1000
+
+void setupRNG();
+int validate(char input[]);
 
 /** 
  * Print random numbers until one is rolled higher than one input
@@ -8,8 +13,27 @@
 
 int main() {
     
-    
+    char input[IN_LIMIT];
+    for(;;) {
+        for(; getline(input);) {
 
+            printf("Input is not a number")
+            getline(input);
+        }
+    }    
+
+}
+
+void setupRNG() {
+    srand(time(NULL));
+}
+
+int validate(char input[]) {
+    for(int i = 0; input[i] != '\0'; ++i) {
+        if(input[i] < '0' || '9' < input[i]) {
+            return 0;
+        }
+    }
 }
 
 //Returns length
