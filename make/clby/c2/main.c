d6f7af59f45fa2cdf2ab2e4d4fcd6cd69bc8e6e6
Luiserebii <luis@serebii.io>
2020-04-04 01:23:45 -0400

    Throwing together 2nd example, learning a bit more about make

---
+++ make/clby/c2/main.c
@@ -0,0 +1,11 @@
+/**
+ * Note the usage of <>s below. Compiling with the -I flag, and signaling
+ * the current directory, will allow this compile to go through.
+ */
+
+#include <hellomake.h>
+
+int main() {
+    hellomake();
+    return 0;
+}
