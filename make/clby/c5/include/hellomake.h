f0d1ceb40e22518726ff64a48e648a30460ea8f7
Luiserebii <luis@serebii.io>
2020-04-04 02:14:46 -0400

    Attempt at constructing 5th example; this a little more difficult to construct

---
+++ make/clby/c5/include/hellomake.h
@@ -0,0 +1,6 @@
+#ifndef HELLOMAKE_H
+#define HELLOMAKE_H
+
+void hellomake(void);
+
+#endif
---
+++ make/clby/c5/src/hellomake.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <hellomake.h>
+
+void hellomake(void) {
+    printf("Hello Make!\n");
+}
